feed0ecd2a132647b0210488b660b7add56c8022
MaTeDii19 <knufeg455@gmail.com>
2023-01-30 19:39:12 +0100

    Initial commit

---
+++ src/main/java/net/MaTeDii/mod/modClient.java
@@ -0,0 +1,10 @@
+package net.MaTeDii.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class modClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
